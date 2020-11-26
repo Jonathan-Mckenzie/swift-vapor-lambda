45b61346a15180e8847f966b8678aa1641b17e76
Jonathan McKenzie <jonathan.mckenzie@zwift.com>
2020-11-26 15:18:09 -0800

    finish all greeting controller tests

--- Tests/AppTests/GreetingTests.swift
+++ Tests/AppTests/GreetingTests.swift
@@ -6,18 +6,67 @@ final class GreetingTests: AppTesting {
     func testShouldHandleRootRequests() throws {
         let app = try prepareApp()
         defer { app.shutdown() }
+
         try app.test(HTTPMethod.GET, getPath(""), afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "hello friend, what is your ?name")
         })
     }
 
+    func testShouldHandleQueryParams() throws {
+        let app = try prepareApp()
+        defer { app.shutdown() }
+
+        let name = "jon"
+        try app.test(HTTPMethod.GET, getPath("") + "?name=\(name)", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "hello \(name)! Try: '/greeting/acceptable'")
+        })
+    }
+
+    func testShouldHandleHeaders() throws {
+        let app = try prepareApp()
+        defer { app.shutdown() }
+
+        let acceptHeaders = "application/custom-type"
+        let contentType = "some-custom-content-type"
+        let headers: HTTPHeaders = HTTPHeaders([
+            ("Accept", acceptHeaders),
+            ("Content-Type", contentType)
+        ])
+        try app.test(
+                HTTPMethod.GET,
+                getPath("acceptable"),
+                headers: headers,
+                afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "you are acceptable; (Accept: \(acceptHeaders)) (Content-Type: \(contentType))")
+        })
+    }
+
+    func testShouldHandleUrlParameters() throws {
+        let app = try prepareApp()
+        defer { app.shutdown() }
+
+        let parameter = "some-id"
+        try app.test(
+            HTTPMethod.GET,
+            getPath(parameter),
+            afterResponse: { res in
+                XCTAssertEqual(res.status, .ok)
+                XCTAssertEqual(res.body.string, "your parameter was: '\(parameter)'")
+        })
+    }
+
     override internal func getPath(_ path: String) -> String {
-        "\(super.getPath(""))/\(GREETING_PATH.description)\(path)"
+        "\(super.getPath(""))/\(GREETING_PATH.description)/\(path)"
     }
 
     static let allTests = [
-        ("testShouldHandleRootRequests", testShouldHandleRootRequests)
+        ("testShouldHandleRootRequests", testShouldHandleRootRequests),
+        ("testShouldHandleQueryParams", testShouldHandleQueryParams),
+        ("testShouldHandleHeaders", testShouldHandleHeaders),
+        ("testShouldHandleUrlParameters", testShouldHandleUrlParameters)
     ]
 }
 
